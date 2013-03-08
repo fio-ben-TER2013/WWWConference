d87d68c88270149fd75638a09e45114582627d76
BenoitDdlp <benoitddlp@msn.com>
2013-03-08 18:15:11 +0100

    fix

--- src/fibe/Bundle/WWWConfBundle/Resources/public/js/XpropertyAdd.js
+++ src/fibe/Bundle/WWWConfBundle/Resources/public/js/XpropertyAdd.js
@@ -2,8 +2,7 @@
 function FormXproperty(id){
 alert(id)
 	$("#idci_bundle_simpleschedulebundle_xpropertytype_calendarEntity").val(id);//Put the id of the last Event create in CalendarEntity
-	 $.post("../schedule/xproperty/"+id+ "/add", $("#uriLink").serialize(),function(data){
-				alert("Xproperty creer ");
+	 $.post("../schedule/xproperty/"+id+ "/add", $("#uriLink").serialize(),function(data){ 
 				$(".alert-success").append("<p>Link created</p>").show(500,function(){
 				    $(".alert-success").fadeOut(3000,function(){
 	                    location.reload();
